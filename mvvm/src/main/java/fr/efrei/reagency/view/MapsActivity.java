4dba878cf5c938fba6d8812c13236a41219ef188
Lay78 <antoine_lay@yahoo.fr>
2021-01-03 02:36:45 +0100

    test

--- mvvm/src/main/java/fr/efrei/reagency/view/MapsActivity.java
+++ mvvm/src/main/java/fr/efrei/reagency/view/MapsActivity.java
@@ -1,32 +1,63 @@
 package fr.efrei.reagency.view;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import fr.efrei.reagency.R;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private HashMap mMarkers;
+    private FusedLocationProviderClient client;
+    private SupportMapFragment mapFragment;
+
+    //get access to location permission
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+
+        getCurrentLocation();
     }
 
     /**
@@ -42,6 +73,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // HashMap which contains all markers of properties
+        mMarkers = new HashMap<Marker, String>();
+
+
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -51,5 +86,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng efrei = new LatLng(48.78879559928572, 2.363705095900726);
         mMap.addMarker(new MarkerOptions().position(efrei).title("Marker in EFREI"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(efrei));
+
+
     }
+
+    // Get the current location
+    private void getCurrentLocation() {
+        //Check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(final Location location) {
+                    // When success
+                    if (location != null){
+                        // Synchonize map
+                        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(GoogleMap googleMap) {
+                                // Initialize latitude and longitude
+                                LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+                                // Create marker options
+                                MarkerOptions options = new MarkerOptions().position(latlng).title("Your current position");
+                                // Zoom map
+                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10));
+                                // Add marker on map
+                                googleMap.addMarker(options);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+        else {
+            // When permission denied
+            // Request permission
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    // Request permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // Call method when permission is granted
+                getCurrentLocation();
+            }
+        }
+    }
+
+    /*@Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    // Permission Denied
+                    Toast.makeText( this,"your message" , Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    //Get location
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public  void getLocation(){
+        //if ( Build.VERSION.SDK_INT >= 23){
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED  ){
+                requestPermissions(new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return ;
+            }
+        //}
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Location myLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (myLocation == null)
+        {
+            myLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+        }
+    }*/
 }
